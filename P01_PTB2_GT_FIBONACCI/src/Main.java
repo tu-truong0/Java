b9bd76d51dd47c0b93397346082d5c9986e12684
tu-truong0 <124219492+tu-truong0@users.noreply.github.com>
2023-02-09 08:58:58 +0700

    Add files via upload

--- P01_PTB2_GT_FIBONACCI/src/Main.java
+++ P01_PTB2_GT_FIBONACCI/src/Main.java
@@ -1,71 +1,56 @@
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) { 
-		float a, b, c;
-		int n;
+		float a = 1, b = 2, c = 1;
+		int n = 121;
+		int x = 5, y = 15;
+
+//		Scanner sc = new Scanner(System.in);
 		
-		Scanner sc = new Scanner(System.in);
-		
+//		System.out.print("Nhap a = ");
+//		a = sc.nextFloat();
+//		
+//		System.out.print("Nhap b = ");
+//		b = sc.nextFloat();
+//		
+//		System.out.print("Nhap c = ");
+//		c = sc.nextFloat();
+		System.out.printf("a = %.2f, b = %.2f, c = %.2f\nn = %d\nx = %d, y = %d\n", a, b, c, n, x, y);
 		System.out.println("--------Chuong trinh giai PT bac 2--------");
-
-		System.out.print("Nhap a = ");
-		a = sc.nextFloat();
-		
-		System.out.print("Nhap b = ");
-		b = sc.nextFloat();
-		
-		System.out.print("Nhap c = ");
-		c = sc.nextFloat();
-
-		
-		Main.PTB2(a, b, c);
-		
-		Main m = new Main();
-		System.out.print("Nhap n = ");
-		n = sc.nextInt();
-		sc.close();
-		
+		Basic.PTB2(a, b, c);
+//		
+//		System.out.print("Nhap n = ");
+//		n = sc.nextInt();
+//		sc.close();
 		System.out.println("--------Chuong trinh tinh n!--------");
-		System.out.println(n + "! = " + m.GT(n));
+		System.out.println(n + "! = " + Basic.GT(n));
 		
 		System.out.println("--------Chuong trinh tinh FIBONACC so thu n--------");
-		System.out.println("fibo(" +n+") = " + m.fibo(n));
+		System.out.println("fibo(" +n+") = " + Basic.Fi(n));
 		
-	}
-	static void PTB2(float a, float b, float c) {
-		float delta = b*b - 4*a*c;
-		if (delta < 0) {
-			System.out.println("PT vo nghiem");
-		}
-		else if (delta == 0) {
-			float x = -b/(2*a);
-			System.out.println("PT co 1 nghiem kep la: x = " + x);
-			}
-		else {
-			float x1 = (float)(-b-Math.sqrt(delta))/(2*a);
-			float x2 = (float)(-b+Math.sqrt(delta))/(2*a);
-			System.out.println("PT co 2 nghiem phan bien");
-			// Dùng String.format 
-			String s= String.format("Nghiem cua PT la: x1 = %f, x2 = %f", x1, x2);
-			System.out.println(s);
-		}
-		}
-	
-	long GT (int n) {
-		if (n<=1)	{
-			return 1;
-		}
-		else {
-			return n * GT(n-1);
-		}
+		System.out.println("--------Uoc chung lon nhat cua x va y--------");
+		System.out.printf("UCLN(%d, %d) = %d\n", x,y, Basic.DQ_UCLN(x, y));
+		
+		System.out.println("--------Boi chung nho nhat cua x va y--------");
+		System.out.printf("BCNN(%d, %d) = %d\n", x,y, Basic.BCNN(x, y));
+		
+		System.out.println("--------In ra n so nguyen to dau tien--------");
+		Basic.Cau6(n);
+		
+		System.out.println("--------Tinh tong cac chu so cua mot so nguyen n--------");
+		System.out.printf("S = %d\n", Basic.Cau8(n));
+		
+		System.out.println("--------Kiem tra tinh chat thuan nghich cua n--------");
+		System.out.println(" co phai la so thuan nghich ?" + Basic.checkThuanNghich(n));
+		/*
+		 * 10. Nhap so tu nhien n. Hay liet ke cac so Fibonacci nho hon n la so nguyen to.
+		 * 11. VCT tong hop nham giai cac bai toan tren, bang cach xay dung giao dien chuong trinh nhu sau
+		 * [1] Phuong trinh bac 2
+		 * [2] Giai thua
+		 * [10]...
+		 * */
 	}
 	
-	long fibo(int n) {
-		if (n==0 || n== 1) return 1;
-		else {
-			return fibo(n-1) + fibo(n-2);
-		}
-	}
+	
+	
 }
