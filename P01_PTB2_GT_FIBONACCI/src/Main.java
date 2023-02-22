c8593458f8a84e20a72e2c6bcbfe05d7cc2c11a7
tu-truong0 <124219492+tu-truong0@users.noreply.github.com>
2023-02-23 06:07:18 +0700

    Update Main.java

--- P01_PTB2_GT_FIBONACCI/src/Main.java
+++ P01_PTB2_GT_FIBONACCI/src/Main.java
@@ -1,23 +1,22 @@
 import java.util.Scanner;
 public class Main {
-
+	static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) { 
 		int n, key;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("[1] Phuong trinh bac 2\n"
-				+ "[2] Giai Thua\n"
-				+ "[3] So Fibonacci\n"
-				+ "[4] UCLN, BCNN\n"
-				+ "[5] Liet Ke Cac So Nguyen To Nho Hon N\n"
-				+ "[6] Liet Ke N So Nguyen To Dau Tien\n"
-				+ "[7] Liet Ke Tat Ca Cac So Nguyen To Co 5 Chu So\n"
-				+ "[8] Tinh Tong Cac Chu So Cua Mot So Nguyen N\n"
-				+ "[9] So Thuan Nghich\n"
-				+ "[10]Liet Ke So Fibonacci\n"
-				+ "[0] Thoat\n"
-				+"");
 		
 		do {
+			System.out.println("\n====CAC BAI TOAN====\n"
+					+ "[1] Phuong trinh bac 2\n"
+					+ "[2] Giai Thua\n"
+					+ "[3] So Fibonacci\n"
+					+ "[4] UCLN, BCNN\n"
+					+ "[5] Liet Ke Cac So Nguyen To Nho Hon N\n"
+					+ "[6] Liet Ke N So Nguyen To Dau Tien\n"
+					+ "[7] Liet Ke Tat Ca Cac So Nguyen To Co 5 Chu So\n"
+					+ "[8] Tinh Tong Cac Chu So Cua Mot So Nguyen N\n"
+					+ "[9] So Thuan Nghich\n"
+					+ "[10]Liet Ke So Fibonacci\n"
+					+"");
 			System.out.println("Chon chuong trinh: ");
 			key = sc.nextInt();
 			switch(key) {
@@ -38,34 +37,48 @@ public class Main {
 					break;
 
 				case 4:
+					long x = 3;
+					long y = 4;
+					System.out.println("UCLN(" + x + ", " + y+ ") = " + Basic.UCLN(x,y) );
+					System.out.println("BCNN(" + x + ", " + y+ ") = " + Basic.BCNN(x,y) );
 					break;
 
 				case 5:
+					n = 30;
+					System.out.println("Liet ke so nguyen to nho hon " +n+": ");
+					Basic.lietKeSNTBeHonN(n);
 					break;
 
 				case 6:
+					n = 5;
+					System.out.println("Liet ke "+n+" SNT dau tien: " );
+					Basic.lietKeNSNTDauTien(n);
 					break;
 
 				case 7:
+					System.out.println("Liet ke tat ca cac SNT co 5 chu so:");
+					Basic.lietKeSNT5ChuSo();
 					break;
 
 				case 8:
+					n = 12345;
+					System.out.println("Tinh tong cac chu so cua so nguyen " + n + ": " + Basic.TONG(n));
 					break;
 
 				case 9:
+					n = 111;
+					System.out.println("Kiem tra tinh thuan nghich cua "+ n+" la: " + Basic.checkThuanNghich(n));
 					break;
 
 				case 10:
+					n = 100;
+					System.out.println("Liet ke so Fibonacci nho hon "+ n + " la SNT");
+					Basic.lietKeSoFiNhoHonNLaSNT(n);
 					break;
-				
-					
-					
 			}
 		}
-			while (key == 0);
+		
+			while (true);
 	
-	sc.close();
 	}
 }
-	
-	
